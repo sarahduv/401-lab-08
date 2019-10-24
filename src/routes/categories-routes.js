0010e08a575371b5382ae103dc36006b94bf4e56
sarahduv <sarah.duvdevani@gmail.com>
2019-10-23 22:25:21 -0700

    test and lint are passing

---
+++ src/routes/categories-routes.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const Categories = require('../models/categories/categories.js');
+const categories = new Categories();
+
+module.exports.register = function(app) {
+  // Routes
+  app.get('/api/v1/categories', getCategories);
+  app.post('/api/v1/categories', postCategories);
+  app.get('/api/v1/categories/:id', getCategory);
+  app.put('/api/v1/categories/:id', putCategories);
+  app.delete('/api/v1/categories/:id', deleteCategories);
+
+  // ROUTE HANDLER FUNCTIONS
+  /**
+   * @params {request} takes in a request
+   * @result {object} returns specified request
+   */
+  function getCategories(request,response,next) {
+    // expects an array of object to be returned from the model
+    categories.get()
+      .then( data => {
+        const output = {
+          count: data.length,
+          results: data,
+        };
+        response.status(200).json(output);
+      })
+      .catch( next );
+  }
+
+  /**
+   * @params {request} takes in a request
+   * @result {object} returns specified request
+   */
+  function getCategory(request,response,next) {
+    // expects an array with the one matching record from the model
+    categories.get(request.params.id)
+      .then( result => response.status(200).json(result[0]) )
+      .catch( next );
+  }
+
+  /**
+   * @params {request} takes in a request
+   * @result {object} creates a new object
+   */
+  function postCategories(request,response,next) {
+    categories.post(request.body)
+      .then( result => response.status(200).json(result) )
+      .catch( next );
+  }
+
+  /**
+   * @params {request} takes in a request
+   * @result {object} updates a specified record
+   */
+  function putCategories(request,response,next) {
+    // expects the record that was just updated in the database
+    categories.put(request.params.id, request.body)
+      .then( result => response.status(200).json(result[0]) )
+      .catch( next );
+  }
+
+  /**
+   * @params {request} takes in a request
+   * @result {object} deletes a specified object by ID
+   */
+  function deleteCategories(request,response,next) {
+    // Expects no return value (resource was deleted)
+    categories.delete(request.params.id)
+      .then( result => response.status(200).json(result) )
+      .catch( next );
+  }
+}
---
+++ src/routes/products-routes.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const Products = require('../models/products/products.js');
+const products = new Products();
+
+module.exports.register = function(app){
+
+  app.get('/api/v1/products', getProducts);
+  app.post('/api/v1/products', postProducts);
+  app.get('/api/v1/products/:id', getProduct);
+  app.put('/api/v1/products/:id', putProducts);
+  app.delete('/api/v1/products/:id', deleteProducts);
+
+
+  // ROUTE HANDLER FUNCTIONS
+  /**
+   * @params {request} takes in a request
+   * @result {object} returns specified request
+   */
+  function getProducts(request,response,next) {
+    // expects an array of objects back
+    products.get()
+      .then( data => {
+        const output = {
+          count: data.length,
+          results: data,
+        };
+        response.status(200).json(output);
+      })
+      .catch( next );
+  }
+
+  /**
+   * @params {request} takes in a request
+   * @result {object} returns specified request
+   */
+  function getProduct(request,response,next) {
+    // expects an array with one object in it
+    products.get(request.params.id)
+      .then( result => response.status(200).json(result[0]) )
+      .catch( next );
+  }
+
+  /**
+   * @params {request} takes in a request
+   * @result {object} creates a new object
+   */
+  function postProducts(request,response,next) {
+    // expects the record that was just added to the database
+    products.post(request.body)
+      .then( result => response.status(200).json(result) )
+      .catch( next );
+  }
+
+  /**
+   * @params {request} takes in a request
+   * @result {object} updates a specified object by ID
+   */
+  function putProducts(request,response,next) {
+    // expects the record that was just updated in the database
+    products.put(request.params.id, request.body)
+      .then( result => response.status(200).json(result) )
+      .catch( next );
+  }
+
+  /**
+   * @params {request} takes in a request
+   * @result {object} deletes an object specified by ID
+   */
+  function deleteProducts(request,response,next) {
+    // Expects no return value (the resource should be gone)
+    products.delete(request.params.id)
+      .then( result => response.status(200).json(result) )
+      .catch( next );
+  }
+}
